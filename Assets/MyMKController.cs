e6463759956ca55894ad0e014577f8bc9507dc76
Akshay <akshay@solovetech.com>
2019-09-04 13:36:15 +0530

    Particle effects added in Main-Copy.unity scene

---
+++ Assets/MyMKController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MK.Glow.Legacy;
+
+public class MyMKController : MonoBehaviour
+{
+    float counter = 3;
+    MKGlowFree mKGlow;
+    float low = 2.5f, up = 5;
+    private void Awake()
+    {
+        mKGlow = GetComponent<MKGlowFree>();
+        StartCoroutine(Fade());
+    }
+    IEnumerator Fade()
+    {
+        while (true)
+        {
+            print("Hii");
+            counter = low;
+            do
+            {
+                counter += Time.deltaTime * 3.5f;
+                mKGlow.bloomIntensity = counter;
+                yield return null;
+            } while (counter < up);
+            counter = up;
+            do
+            {
+                counter -= Time.deltaTime * 3.5f;
+                mKGlow.bloomIntensity = counter;
+                yield return null;
+            } while (counter > low);
+        }
+    }
+}
